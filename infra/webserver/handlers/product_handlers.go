14bc2caba3dde21cb52f7bbe72feafe5a08edb92
eron97 <eronsantosbetine@gmail.com>
2024-05-19 13:06:21 -0300

    add functions handlers

--- infra/webserver/handlers/product_handlers.go
+++ infra/webserver/handlers/product_handlers.go
@@ -1,1 +1,43 @@
 package handlers
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/eron97/project-fullCycle.git/infra/database"
+	"github.com/eron97/project-fullCycle.git/internal/dto"
+	"github.com/eron97/project-fullCycle.git/internal/entity"
+)
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{
+		ProductDB: db,
+	}
+}
+
+func (h *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var product dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&product)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	p, err := entity.NewProduct(product.Name, product.Price)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	err = h.ProductDB.Create(p)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(err)
+}
